50d81c4484226b8e54f60c394024f78785e33310
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-08-16 23:54:31 +0400

    as

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -1,5 +1,6 @@
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore deneme 1");
+        System.out.println("Gitignore deneme Branch");
     }
 }
